8ab7c3381d0eedf9ea269ab7eefa494336293e95
aaron <aaron@aarons-27-imac.bidmc.harvard.edu>
2019-05-01 13:48:05 -0400

    mac compatibility

--- src/main/java/ajs/joglcanvas/JOGLImageCanvas.java
+++ src/main/java/ajs/joglcanvas/JOGLImageCanvas.java
@@ -422,7 +422,8 @@ public class JOGLImageCanvas extends ImageCanvas implements GLEventListener, Ima
 
 		//drawing
 		gl.glDisable(GL_SCISSOR_TEST);
-		gl.glDrawBuffers(1, new int[] {GL_BACK_LEFT},0);
+		//gl.glDrawBuffers(1, new int[] {GL_BACK_LEFT},0);
+		//gl.glDrawBuffer(GL_BACK_LEFT);
 		gl.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		gl.glClearBufferfv(GL_COLOR, 0, new float[] {0f,0f,0f,0f},0);
         gl.glClearBufferfv(GL_DEPTH, 0, new float[] {0f},0);
@@ -434,7 +435,8 @@ public class JOGLImageCanvas extends ImageCanvas implements GLEventListener, Ima
 			if(go3d) {
 				if(stereoType==StereoType.QUADBUFFER) {
 					if(stereoi==1)
-						gl.glDrawBuffers(1, new int[] {GL_BACK_RIGHT},0);
+						gl.glDrawBuffer(GL_RIGHT);
+						//gl.glDrawBuffers(1, new int[] {GL_BACK_RIGHT},0);
 				}else if(stereoType==StereoType.CARDBOARD) {
 					float[] ortho = FloatUtil.makeOrtho(new float[16], 0, false, -CB_MAXSIZE, CB_MAXSIZE, -CB_MAXSIZE*yrat, CB_MAXSIZE*yrat, -CB_MAXSIZE, CB_MAXSIZE);
 					float[] translate=FloatUtil.makeTranslation(new float[16], 0, false, (stereoi==0?(-CB_MAXSIZE*CB_TRANSLATE):(CB_MAXSIZE*CB_TRANSLATE)), 0f, 0f);

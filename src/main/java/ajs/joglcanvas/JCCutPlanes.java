0f205ccf482ec501779d0d63a6474590e59ae732
aschain <aschain@users.noreply.github.com>
2020-02-14 17:02:29 -0500

    Cutplanes nicer

--- src/main/java/ajs/joglcanvas/JCCutPlanes.java
+++ src/main/java/ajs/joglcanvas/JCCutPlanes.java
@@ -1,13 +1,17 @@
 package ajs.joglcanvas;
 
+import java.awt.Button;
 import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.Label;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.AdjustmentEvent;
 
+import ajs.joglcanvas.JCAdjuster.NumberScrollPanel;
 import ajs.joglcanvas.JOGLImageCanvas.FloatCube;
 
 @SuppressWarnings("serial")
@@ -15,6 +19,7 @@ public class JCCutPlanes extends JCAdjuster {
 	
 	FloatCube c;
 	private final static char[] cps=new char[] {'X','Y','Z','W','H','D'};
+	private NumberScrollPanel[] nsps=new NumberScrollPanel[cps.length];
 
 	public JCCutPlanes(JOGLImageCanvas jic) {
 		super("Cut Planes", jic);
@@ -29,18 +34,42 @@ public class JCCutPlanes extends JCAdjuster {
 		c.anchor=GridBagConstraints.CENTER;
 		c.insets=new Insets(5,5,5,5);
 		add(new Label("Cut Planes"),c);
-		for(int i=0;i<6;i++) {
+		for(int i=0;i<cps.length;i++) {
 			c.gridy++;
-			NumberScrollPanel nsp=new NumberScrollPanel(inits[i],((i>2)?1:0),whd[(i>2)?(i-3):i]+(i>2?1:0),cps[i],0);
-			add(nsp, c);
-			nsp.addAdjustmentListener(this);
-			nsp.setFocusable(false);
+			nsps[i]=new NumberScrollPanel(inits[i],((i>2)?1:0),whd[(i>2)?(i-3):i]+(i>2?1:0),cps[i],0);
+			add(nsps[i], c);
+			nsps[i].addAdjustmentListener(this);
+			nsps[i].setFocusable(false);
 		}
+		c.gridy++;
+		Button b=new Button("Reset");
+		b.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for(int i=0;i<3;i++) {
+					nsps[i].setFloatValue(0);
+					nsps[i+3].setFloatValue((i==0?imp.getWidth():(i==1?imp.getHeight():imp.getNSlices())));
+				}
+				update();
+			}
+		});
+		add(b,c);
 		pack();
 		Container win=jic.icc.getParent();
 		Point loc=win.getLocation();
 		setLocation(new Point(loc.x+win.getSize().width+10,loc.y+5));
 		show();
+	}
+	
+	private void update() {
+		int i=0;
+		c.x=nsps[i++].getFloatValue();
+		c.y=nsps[i++].getFloatValue();
+		c.z=nsps[i++].getFloatValue();
+		c.w=nsps[i++].getFloatValue();
+		c.h=nsps[i++].getFloatValue();
+		c.d=nsps[i++].getFloatValue();
+		jic.setCutPlanesCube(c);
 	}
 
 	@Override
@@ -51,26 +80,26 @@ public class JCCutPlanes extends JCAdjuster {
 			NumberScrollPanel nsp=(NumberScrollPanel)source;
 			switch(nsp.getLabel()) {
 			case 'X':
-				c.x=(float)nsp.getValue();
+				if(nsp.getFloatValue()>=nsps[3].getFloatValue())nsps[3].setFloatValue(nsp.getFloatValue()+1f);
 				break;
 			case 'Y':
-				c.y=(float)nsp.getValue();
+				if(nsp.getFloatValue()>=nsps[4].getFloatValue())nsps[4].setFloatValue(nsp.getFloatValue()+1f);
 				break;
 			case 'Z':
-				c.z=(float)nsp.getValue();
+				if(nsp.getFloatValue()>=nsps[5].getFloatValue())nsps[5].setFloatValue(nsp.getFloatValue()+1f);
 				break;
 			case 'W':
-				c.w=(float)nsp.getValue();
+				if(nsp.getFloatValue()<=nsps[0].getFloatValue())nsps[0].setFloatValue(nsp.getFloatValue()-1f);
 				break;
 			case 'H':
-				c.h=(float)nsp.getValue();
+				if(nsp.getFloatValue()<=nsps[1].getFloatValue())nsps[1].setFloatValue(nsp.getFloatValue()-1f);
 				break;
 			case 'D':
-				c.d=(float)nsp.getValue();
+				if(nsp.getFloatValue()<=nsps[2].getFloatValue())nsps[2].setFloatValue(nsp.getFloatValue()-1f);
 				break;
 			}
-			jic.setCutPlanesCube(c);
 		}
+		update();
 
 	}
 }
